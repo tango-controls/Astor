fd987d1bee52b8502af992c022247231a8f2c9ce
taurel <taurel@4e9c00fd-8f2e-0410-aa12-93ce3db5e235>
2011-02-22 09:31:46 +0000

    Move astor directory to tools

---
+++ ReleaseNote.java
@@ -0,0 +1,53 @@
+
+package admin.astor;
+
+public interface ReleaseNote
+{
+	public final String	str = 
+		"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 3.2//EN\">\n" + 
+		"<HTML>\n" + 
+		"<HEAD>\n" + 
+		"<Title> Release Note </Title>\n" + 
+		"</HEAD>\n" + 
+		"<BODY TEXT=\"#000000\" BGCOLOR=\"#FFFFFF\" LINK=\"#0000FF\" VLINK=\"#7F00FF\" ALINK=\"#FF0000\">\n" + 
+		"<P><!-------TITLE------></P>\n" + 
+		"<Center>	<h2>Astor Release Note</h2>\n" + 
+		"	(Generated Fri Jan 13 15:15:50 CET 2006)</Center><Br>\n" + 
+		"<li><b>Astor-4.2.0:</b><Br>\n" + 
+		"&nbsp; &nbsp; Polling profler added.<Br>\n" + 
+		"<li><b>Astor-4.1.4:</b><Br>\n" + 
+		"&nbsp; &nbsp; Open/Save menu added on event tester window.<Br>\n" + 
+		"<li><b>Astor-4.1.3:</b><Br>\n" + 
+		"&nbsp; &nbsp; History added on event management.<Br>\n" + 
+		"&nbsp; &nbsp; &nbsp; &nbsp; The maximum servers displayed in horizontal on HostInfoDialog<Br>\n" + 
+		"&nbsp; &nbsp; &nbsp; &nbsp; window has been set to 5.<Br>\n" + 
+		"<li><b>Astor-4.1.2:</b><Br>\n" + 
+		"&nbsp; &nbsp; Bug fixed in Device browser in member device name.<Br>\n" + 
+		"<li><b>Astor-4.1.1:</b><Br>\n" + 
+		"&nbsp; &nbsp; Change TANGO_HOST added (needs TangORB-4.7.7 or later).<Br>\n" + 
+		"<li><b>Astor-4.1.0:</b><Br>\n" + 
+		"&nbsp; &nbsp; DevBrowser and MkStarter utilities added.<Br>\n" + 
+		"<li><b>Astor-4.0.9:</b><Br>\n" + 
+		"&nbsp; &nbsp; Minor changes for EventTester compatibility<Br>\n" + 
+		"<li><b>Astor-4.0.8:</b><Br>\n" + 
+		"&nbsp; &nbsp; Screen position modified for dialogs.<Br>\n" + 
+		"<li><b>Astor-4.0.7:</b><Br>\n" + 
+		"&nbsp; &nbsp; Search if host already exist before creation.<Br>\n" + 
+		"<li><b>Astor-4.0.6:</b><Br>\n" + 
+		"&nbsp; &nbsp; Search by host name added.<Br>\n" + 
+		"<li><b>Astor-4.0.5:</b><Br>\n" + 
+		"&nbsp; &nbsp; Can change memorized attribute value.<Br>\n" + 
+		"<li><b>Astor-4.0.4:</b><Br>\n" + 
+		"&nbsp; &nbsp; Bug in server architecture fixed.<Br>\n" + 
+		"<li><b>Astor-4.0.3:</b><Br>\n" + 
+		"&nbsp; &nbsp; Minor changes, RemoteCmd property added.<Br>\n" + 
+		"<li><b>Astor-4.0.2:</b><Br>\n" + 
+		"&nbsp; &nbsp; Server architecture display addded.<Br>\n" + 
+		"<li><b>Astor-4.0.1:</b><Br>\n" + 
+		"&nbsp; &nbsp; DevWizard calls addded.<Br>\n" + 
+		"<li><b>Astor-4.0.0:</b><Br>\n" + 
+		"&nbsp; &nbsp; Possibility to controle two database servers added.<Br>\n" + 
+		"</Body>\n" + 
+		"</Html>\n" + 
+		"";
+}
