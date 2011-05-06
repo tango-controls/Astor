5604e2fd0a31f63ef660ee9480227d79d37994b2
pascal_verdier <pascal_verdier@4e9c00fd-8f2e-0410-aa12-93ce3db5e235>
2011-05-06 09:26:27 +0000

    Change header on source files.

--- tools/astor/trunk/admin/astor/access/EditTreeItem.java
+++ tools/astor/trunk/admin/astor/access/EditTreeItem.java
@@ -1,30 +1,36 @@
 //+======================================================================
-// $Source$
+// $Source:  $
 //
 // Project:   Tango
 //
-// Description:  java source code for the Astor class definition .
+// Description:  java source code for Tango manager tool..
 //
 // $Author$
 //
+// Copyright (C) :      2004,2005,2006,2007,2008,2009,2010,2011
+//						European Synchrotron Radiation Facility
+//                      BP 220, Grenoble 38043
+//                      FRANCE
+//
+// This file is part of Tango.
+//
+// Tango is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Tango is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Tango.  If not, see <http://www.gnu.org/licenses/>.
+//
 // $Revision$
 //
-// $Log$
-// Revision 1.3  2008/11/19 10:01:34  pascal_verdier
-// New tests done on Access control.
-// Allowed commands tree added.
-//
-// Revision 1.2  2006/10/02 14:09:02  pascal_verdier
-// Minor changes.
-//
-// Revision 1.1  2006/09/19 13:06:47  pascal_verdier
-// Access control manager added.
-//
-//
-//
-// Copyleft 2003 by European Synchrotron Radiation Facility, Grenoble, France
-//               All Rights Reversed
 //-======================================================================
+
 
 package admin.astor.access;
 
--- tools/astor/trunk/admin/astor/access/UsersTree.java
+++ tools/astor/trunk/admin/astor/access/UsersTree.java
@@ -1,35 +1,36 @@
 //+======================================================================
-// $Source$
+// $Source:  $
 //
 // Project:   Tango
 //
-// Description:	java source code for display JTree
+// Description:  java source code for Tango manager tool..
 //
 // $Author$
 //
+// Copyright (C) :      2004,2005,2006,2007,2008,2009,2010,2011
+//						European Synchrotron Radiation Facility
+//                      BP 220, Grenoble 38043
+//                      FRANCE
+//
+// This file is part of Tango.
+//
+// Tango is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Tango is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Tango.  If not, see <http://www.gnu.org/licenses/>.
+//
 // $Revision$
 //
-// $Log$
-// Revision 1.5  2011/01/04 14:34:18  pascal_verdier
-// Password added for access control dialog.
-//
-// Revision 1.4  2009/02/18 09:51:46  pascal_verdier
-// Splah screen management moved to AstorUtil class.
-//
-// Revision 1.3  2008/11/19 10:01:34  pascal_verdier
-// New tests done on Access control.
-// Allowed commands tree added.
-//
-// Revision 1.2  2006/10/02 14:09:02  pascal_verdier
-// Minor changes.
-//
-// Revision 1.1  2006/09/19 13:06:47  pascal_verdier
-// Access control manager added.
-//
-//
-// copyleft 2006 by European Synchrotron Radiation Facility, Grenoble, France
-//							 All Rights Reversed
 //-======================================================================
+
 
 package admin.astor.access;
 
