5be253dac1cc441d376c9e22875f433730a57b25
Pascal Verdier <verdier@esrf.fr>
2016-12-15 08:59:29 +0100

    improve update file

--- src/main/java/admin/astor/Astor.java
+++ src/main/java/admin/astor/Astor.java
@@ -67,7 +67,7 @@ public class Astor extends JFrame implements AstorDefs {
      * Initialized by make jar call and used to display title.
      */
     private static String revNumber =
-            "7.0.6  -  15-12-2016  08:03:54";
+            "7.0.6  -  15-12-2016  08:58:06";
     /**
      * JTree object to display control system.
      */
